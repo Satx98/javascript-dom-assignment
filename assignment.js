52c0b7154ab854113b7cf08a6a68081e802c22ca
Satadru Roy <satadru.roy98@gmail.com>
2020-07-04 14:37:28 +0530

    Assignment solved

---
+++ assignment.js
@@ -0,0 +1,13 @@
+const liTask11 = document.getElementById('task-1');
+const liTask12 = document.querySelector('#task-1');
+
+liTask11.style.backgroundColor = 'black';
+liTask12.style.color = 'white';
+
+const headTitle = document.querySelector('title');
+const head = document.head;
+const headTitle1 = head.querySelector('title');
+headTitle1.textContent = 'Assignment - Solved!';
+
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment - Solved!';
