823cc3f3f2d89b12772cb6c4e5f42b0c3b3d76b7
Satadru Roy <satadru.roy98@gmail.com>
2020-07-04 14:52:37 +0530

    Changes made to follow along with the Instructor - Assignment complete

--- assignment.js
+++ assignment.js
@@ -1,13 +1,17 @@
-const liTask11 = document.getElementById('task-1');
-const liTask12 = document.querySelector('#task-1');
+const task1El1 = document.getElementById('task-1');
+const task1El2 = document.querySelector('li');
 
-liTask11.style.backgroundColor = 'black';
-liTask12.style.color = 'white';
+task1El1.style.color = 'white';
+task1El2.style.backgroundColor = 'black';
 
-const headTitle = document.querySelector('title');
-const head = document.head;
-const headTitle1 = head.querySelector('title');
-headTitle1.textContent = 'Assignment - Solved!';
+const docTitle1 = document.querySelector('title');
+// docTitle1.textContent = 'Assignment - Solved!';
 
-const h1 = document.querySelector('h1');
-h1.textContent = 'Assignment - Solved!';
+const docHead = document.head;
+const docTitle2 = document.head.querySelector('title');
+
+docTitle2.textContent = 'Assignment - Solved!';
+
+// const h1 = document.querySelector('h1');
+const h1 = document.getElementsByTagName('h1');
+h1[0].textContent = 'Assignment - Solved!';
